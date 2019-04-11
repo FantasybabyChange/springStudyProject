117c4cd5f690345ee8589fe9fb2f6a57c172ae44
fantasybaby <625353861@qq.com>
2019-04-11 23:55:18 +0800

    convert to maven

--- spring-hello/src/main/java/com/fantasybaby/hello/SpringHelloApplication.java
+++ spring-hello/src/main/java/com/fantasybaby/hello/SpringHelloApplication.java
@@ -3,6 +3,7 @@ package com.fantasybaby.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,8 +13,9 @@ public class SpringHelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringHelloApplication.class, args);
 	}
-	@RequestMapping("/hello")
+	@RequestMapping(method = RequestMethod.GET,value = "/hello")
 	public String hello(){
-		return "done";
+		return "hello";
 	}
+
 }
