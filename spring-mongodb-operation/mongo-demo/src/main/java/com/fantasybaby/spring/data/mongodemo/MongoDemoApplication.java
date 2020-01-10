cc2916ba28aa0ffc6a8c1f54873a33c1edf8d9b2
fantasybaby <625353861@qq.com>
2020-01-10 18:39:41 +0800

    add mongo test

---
+++ spring-mongodb-operation/mongo-demo/src/main/java/com/fantasybaby/spring/data/mongodemo/MongoDemoApplication.java
@@ -0,0 +1,73 @@
+package com.fantasybaby.spring.data.mongodemo;
+
+import com.fantasybaby.spring.data.mongodemo.converter.MoneyReadConverter;
+import com.fantasybaby.spring.data.mongodemo.model.Coffee;
+import com.mongodb.client.result.UpdateResult;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@SpringBootApplication
+@Slf4j
+public class MongoDemoApplication implements ApplicationRunner {
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDemoApplication.class, args);
+	}
+
+	@Bean
+	public MongoCustomConversions mongoCustomConversions() {
+		return new MongoCustomConversions(Arrays.asList(new MoneyReadConverter()));
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		List<Coffee> coffees = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			Coffee espresso = Coffee.builder()
+					.name("espresso"+i)
+					.price(Money.of(CurrencyUnit.of("CNY"), 20.0))
+					.createTime(new Date())
+					.updateTime(new Date()).build();
+			coffees.add(espresso);
+		}
+		Collection<Coffee> coffees1 = mongoTemplate.insertAll(coffees);
+//		log.info("Coffee {}", saved);
+
+		List<Coffee> list = mongoTemplate.find(
+				Query.query(Criteria.where("name").is("espresso")), Coffee.class);
+		log.info("Find {} Coffee", list.size());
+		list.forEach(c -> log.info("Coffee {}", c));
+
+		Thread.sleep(1000); // 为了看更新时间
+		UpdateResult result = mongoTemplate.updateFirst(Query.query(Criteria.where("name").is("espresso")),
+				new Update().set("price", Money.ofMajor(CurrencyUnit.of("CNY"), 30))
+						.currentDate("updateTime"),
+				Coffee.class);
+		log.info("Update Result: {}", result.getModifiedCount());
+		Coffee updateOne = mongoTemplate.findById(list.get(0).getId(), Coffee.class);
+		log.info("Update Result: {}", updateOne);
+
+//		mongoTemplate.remove(updateOne);
+	}
+}
+
