6a232220b06a0f9ac72fd2af77717d2031890040
Reid Liu <Reid.liu@kuka.com>
2022-01-18 17:50:57 +0800

    add work note

---
+++ spring-cloud-study/zk-customer-service/src/main/java/com/fantasybaby/spring/cloud/zk/customer/model/NewOrderRequest.java
@@ -0,0 +1,16 @@
+package com.fantasybaby.spring.cloud.zk.customer.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+public class NewOrderRequest {
+    private String customer;
+    private List<String> items;
+}
---
+++ spring-cloud-study/zk-customer-service/src/main/java/com/fantasybaby/spring/cloud/zk/customer/model/OrderState.java
@@ -0,0 +1,5 @@
+package com.fantasybaby.spring.cloud.zk.customer.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
