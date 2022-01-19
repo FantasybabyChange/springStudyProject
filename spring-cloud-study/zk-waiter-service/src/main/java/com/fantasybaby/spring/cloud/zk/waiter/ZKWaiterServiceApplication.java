02d167eccb6d1e14a4f04e1c8b1bfb568ba0a7a3
Reid Liu <Reid.liu@kuka.com>
2022-01-19 13:35:39 +0800

    change to zk

--- spring-cloud-study/zk-waiter-service/src/main/java/com/fantasybaby/spring/cloud/zk/waiter/ZKWaiterServiceApplication.java
+++ spring-cloud-study/zk-waiter-service/src/main/java/com/fantasybaby/spring/cloud/zk/waiter/ZKWaiterServiceApplication.java
@@ -18,10 +18,10 @@ import java.util.TimeZone;
 @EnableJpaRepositories
 @EnableCaching
 @EnableDiscoveryClient
-public class WaiterServiceApplication implements WebMvcConfigurer {
+public class ZKWaiterServiceApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WaiterServiceApplication.class, args);
+		SpringApplication.run(ZKWaiterServiceApplication.class, args);
 	}
 
 	@Override
