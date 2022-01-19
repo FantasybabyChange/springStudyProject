2da50ea9e90eb5949eb024d6ac80ab7c04b85f64
Reid Liu <Reid.liu@kuka.com>
2022-01-19 16:35:02 +0800

    project

---
+++ spring-cloud-study/consul-waiter-service/src/main/java/com/fantasybaby/spring/cloud/consul/waiter/repository/CoffeeRepository.java
@@ -0,0 +1,11 @@
+package com.fantasybaby.spring.cloud.consul.waiter.repository;
+
+import com.fantasybaby.spring.cloud.consul.waiter.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    List<Coffee> findByNameInOrderById(List<String> list);
+    Coffee findByName(String name);
+}
