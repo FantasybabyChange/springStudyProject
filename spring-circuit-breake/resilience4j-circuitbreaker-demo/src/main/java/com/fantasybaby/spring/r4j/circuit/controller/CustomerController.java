5bd5367d01f5cef45a15a49ce3fb5294aca19111
Reid Liu <Reid.liu@kuka.com>
2022-02-10 18:04:41 +0800

    add resilience4j

---
+++ spring-circuit-breake/resilience4j-circuitbreaker-demo/src/main/java/com/fantasybaby/spring/r4j/circuit/controller/CustomerController.java
@@ -0,0 +1,57 @@
+package com.fantasybaby.spring.r4j.circuit.controller;
+
+import com.fantasybaby.spring.r4j.circuit.integration.CoffeeOrderService;
+import com.fantasybaby.spring.r4j.circuit.integration.CoffeeService;
+import com.fantasybaby.spring.r4j.circuit.model.Coffee;
+import com.fantasybaby.spring.r4j.circuit.model.CoffeeOrder;
+import com.fantasybaby.spring.r4j.circuit.model.NewOrderRequest;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.control.Try;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+@Slf4j
+public class CustomerController {
+    @Autowired
+    private CoffeeService coffeeService;
+    @Autowired
+    private CoffeeOrderService coffeeOrderService;
+    private CircuitBreaker circuitBreaker;
+
+    public CustomerController(CircuitBreakerRegistry registry) {
+        circuitBreaker = registry.circuitBreaker("menu");
+    }
+
+    @GetMapping("/menu")
+    public List<Coffee> readMenu() {
+        return Try.ofSupplier(
+                CircuitBreaker.decorateSupplier(circuitBreaker,
+                        () -> coffeeService.getAll()))
+                .recover(CallNotPermittedException.class, Collections.emptyList())
+                .get();
+    }
+
+    @PostMapping("/order")
+    @io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker(name = "order")
+    public CoffeeOrder createOrder() {
+        NewOrderRequest orderRequest = NewOrderRequest.builder()
+                .customer("Li Lei")
+                .items(Arrays.asList("capuccino"))
+                .build();
+        CoffeeOrder order = coffeeOrderService.create(orderRequest);
+        log.info("Order ID: {}", order != null ? order.getId() : "-");
+        return order;
+    }
+}
---
+++ spring-circuit-breake/resilience4j-circuitbreaker-demo/src/main/java/com/fantasybaby/spring/r4j/circuit/model/OrderState.java
@@ -0,0 +1,5 @@
+package com.fantasybaby.spring.r4j.circuit.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
