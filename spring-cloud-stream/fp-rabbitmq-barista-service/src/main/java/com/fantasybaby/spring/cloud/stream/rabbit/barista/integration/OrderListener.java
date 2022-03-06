23366ccf0603f149d1c6ae4487af0cd53a2eac0f
fantasybaby <625353861@qq.com>
2022-03-06 12:16:54 +0800

    add fp project

--- spring-cloud-stream/fp-rabbitmq-barista-service/src/main/java/com/fantasybaby/spring/cloud/stream/rabbit/barista/integration/OrderListener.java
+++ spring-cloud-stream/fp-rabbitmq-barista-service/src/main/java/com/fantasybaby/spring/cloud/stream/rabbit/barista/integration/OrderListener.java
@@ -1,46 +1,30 @@
 package com.fantasybaby.spring.cloud.stream.rabbit.barista.integration;
 
-import com.fantasybaby.spring.cloud.stream.rabbit.barista.model.CoffeeOrder;
-import com.fantasybaby.spring.cloud.stream.rabbit.barista.model.OrderState;
-import com.fantasybaby.spring.cloud.stream.rabbit.barista.repository.CoffeeOrderRepository;
+import com.fantasybaby.spring.cloud.stream.rabbit.barista.service.IOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.function.Function;
 
 @Component
 @Slf4j
-@Transactional
+//@Transactional
 public class OrderListener {
-    @Autowired
-    private CoffeeOrderRepository orderRepository;
+
     @Autowired
     private StreamBridge streamBridge;
-    @Value("${order.barista-prefix}${random.uuid}")
-    private String barista;
+    @Resource
+    private IOrderService orderService;
 
     @Bean
     public Function<Long, String> newOrders() {
         return id -> {
-            CoffeeOrder o = orderRepository.getById(id);
-            if (o == null) {
-                log.warn("Order id {} is NOT valid.", id);
-                return "hello";
-            }
-            log.info("Receive a new Order {}. Waiter: {}. Customer: {}",
-                    id, o.getWaiter(), o.getCustomer());
-            o.setState(OrderState.BREWED);
-            o.setBarista(barista);
-            orderRepository.save(o);
+            orderService.getCoffeeOrder(id);
             log.info("Order {} is READY.", id);
             streamBridge.send("finishedOrders-out-0", MessageBuilder.withPayload(id).build());
             return "hello";
